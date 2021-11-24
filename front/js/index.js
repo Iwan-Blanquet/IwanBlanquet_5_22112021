d6ebfef21573d72dbe31c97efd6f5ecad9e3c265
Iwan-Blanquet <blanquetiwan@gmail.com>
2021-11-24 14:40:45 +0100

    API Request

---
+++ front/js/index.js
@@ -0,0 +1,18 @@
+/*
+    AFFICHER les produits sur la page d'accueil
+*/
+
+fetch("http://localhost:3000/api/products")
+    .then(dataProduct => dataProduct.json())
+    .then(jsonListProduct => {
+        for(let product of jsonListProduct) {
+            document.querySelector(".items").innerHTML = document.querySelector(".items").innerHTML + 
+                                                        `<a href="./product.html?id=${product._id}">
+                                                            <article>
+                                                            <img src="${product.imageUrl}" alt="${product.altTxt}, ${product.name}">
+                                                            <h3 class="productName">${product.name}</h3>
+                                                            <p class="productDescription">${product.description}</p>
+                                                            </article>
+                                                         </a>`
+        }
+    });

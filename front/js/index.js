fbba713e712a0c209c6a464a759b9b3a8d5e803f
Iwan-Blanquet <blanquetiwan@gmail.com>
2021-11-25 19:26:57 +0100

    Mise en forme html des données de l'API

--- front/js/index.js
+++ front/js/index.js
@@ -1,17 +1,39 @@
 /*
-    AFFICHER les produits sur la page d'accueil
+    AFFICHER et GERER les produits sur la page d'accueil
 */
 
+// Requête API 
+let root =  document.querySelector(".items");
 fetch("http://localhost:3000/api/products")
     .then(dataProduct => dataProduct.json())
     .then(jsonListProduct => {
         for(let product of jsonListProduct) {
-            document.querySelector(".items").innerHTML += `<a href="./product.html?id=${product._id}">
-                                                            <article>
-                                                            <img src="${product.imageUrl}" alt="${product.altTxt}, ${product.name}">
-                                                            <h3 class="productName">${product.name}</h3>
-                                                            <p class="productDescription">${product.description}</p>
-                                                            </article>
-                                                         </a>`
+            loadProduct(product);
         }
-    });
+    })
+
+    // Créer et gérer les balises html pour les produits
+function loadProduct(product) {
+    let a = document.createElement('a');
+    a.href = `./product.html?id=${product._id}`;
+
+    let article = document.createElement('article');
+
+    let img = document.createElement('img');
+    img.src = product.imageUrl;
+    img.alt = product.altTxt;
+
+    let h3 = document.createElement('h3');
+    h3.innerHTML = product.name;
+    h3.classList = "productName";
+
+    let p = document.createElement('p');
+    p.innerHTML = product.description;
+    p.classList = "productDescription";
+
+    article.appendChild(img);
+    article.appendChild(h3);
+    article.appendChild(p);
+    a.appendChild(article);
+    root.appendChild(a);
+}
--- front/js/product.js
+++ front/js/product.js
@@ -2,15 +2,14 @@
     GERER et AFFICHER les informations des Produits sur leurs propres pages
 */
 
-//Erreur dans le code
-fetch("http://localhost:3000/api/products")
-    .then(dataProduct => dataProduct.json())
-    .then(product => {
-        console.log(product)
-            // let image =document.createElement('img');
-            // image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}" />`;
-            // document.getElementsByClassName("item_img").appendChild(image);
-    });
-// let image =document.createElement('img');
+//Refaire un fetch avec GET id ?
+
+// let product = ; RECUPERER l'id du produit
+
+// let image = document.createElement('img');
 // image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}" />`;
-// document.getElementsByClassName("item_img").append(image);
+// document.querySelector("item__img").appendChild(image);
+
+let parsedUrl = new URL(window.location.href);
+console.log(window.location.href)
+console.log(parsedUrl.searchParams.get("id"))

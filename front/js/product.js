a3ed4d7ca405109e66c7320228e4c24fa5d2e490
Iwan-Blanquet <blanquetiwan@gmail.com>
2021-11-29 08:38:03 +0100

    find URL id

--- front/js/product.js
+++ front/js/product.js
@@ -2,14 +2,13 @@
     GERER et AFFICHER les informations des Produits sur leurs propres pages
 */
 
-//Refaire un fetch avec GET id ?
-
-// let product = ; RECUPERER l'id du produit
+//URLSearchParams
 
 // let image = document.createElement('img');
 // image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}" />`;
 // document.querySelector("item__img").appendChild(image);
 
-let parsedUrl = new URL(window.location.href);
-console.log(window.location.href)
-console.log(parsedUrl.searchParams.get("id"))
+let productUrl = new URL(window.location.href);
+let productId = productUrl.searchParams.get("id");
+
+
